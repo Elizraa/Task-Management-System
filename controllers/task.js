59b6cc2c866f8a13c2b07f0e6ccad65bb0f0db70
Azriel <azrielwah@gmail.com>
2023-12-21 23:32:23 +0700

    [add]: new api update task

--- controllers/task.js
+++ controllers/task.js
@@ -1,28 +1,33 @@
 const { Task, Status, status_index, } = require('../models/task');
-const { create_task_schema } = require('../schema/task')
+const { create_task_schema, update_task_schema } = require('../schema/task')
 
 const get_all_tasks = async (req, res) => {
   try {
-    const samples = await Task.findAll();
-    res.json(samples);
+    const tasks = await Task.findAll();
+    const mapped_status_tasks = tasks.map(task => ({
+      ...task.dataValues,
+      status: status_index[task.dataValues.status],
+    }));
+    
+    res.json(mapped_status_tasks);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 const create_task = async (req, res) => {
-  const { error, value } = create_task_schema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ error: error.details[0].message });
-  }
-
-  const task = new Task({
-    title: value.title,
-    description: value.description,
-    status: status_index.indexOf(value.status)
-  });
-
   try {
+    const { error, value } = create_task_schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const task = new Task({
+      title: value.title,
+      description: value.description,
+      status: status_index.indexOf(value.status)
+    });
+
     const new_task = await task.save();
     res.status(201).json({inserted_task_id:new_task.dataValues.id});
   } catch (error) {
@@ -31,9 +36,9 @@ const create_task = async (req, res) => {
 };
 
 const remove_task = async (req, res) => {
-  const task_id = req.params.task_id;
-
   try {
+    const task_id = req.params.task_id;
+
     const deleted_task = await Task.destroy({ where: { id: task_id } });
     if (!deleted_task) {
       return res.status(404).json({ message: 'Task not found' });
@@ -44,4 +49,28 @@ const remove_task = async (req, res) => {
   }
 };
 
-module.exports = { get_all_tasks, create_task, remove_task };
+const update_task = async (req, res) => {
+  try {
+    const task_id = req.params.task_id;
+    const task = await Task.findByPk(task_id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    
+    const { error, value } = update_task_schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    if (value.title) task.title = value.title;
+    if (value.description != undefined) task.description = value.description;
+    if (value.status !== undefined) task.status = status_index.indexOf(value.status);
+
+    await task.save();
+    res.status(200).json({ message: 'Task updated successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { get_all_tasks, create_task, remove_task, update_task };
--- routes/task.js
+++ routes/task.js
@@ -6,6 +6,6 @@ const task_controller = require('../controllers/task');
 router.get('/', task_controller.get_all_tasks);
 router.post('/', task_controller.create_task);
 router.delete('/:task_id', task_controller.remove_task);
-// router.put('/:task_id', task_controller.update_task);
+router.put('/:task_id', task_controller.update_task);
 
 module.exports = router;
